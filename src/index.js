8dd0d91a0dba9a78fb43119a947782c43f304ecb
ashenfield98 <ashley.enfield98@gmail.com>
2026-01-31 10:04:10 +0100

    Added typewriter plugin and javascript

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: "here is your poem",
+    autoStart: true,
+    delay: 85,
+    cursor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
