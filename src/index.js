bd200eeae77039b848dd6c10d037b63c3a1b6eee
ashenfield98 <ashley.enfield98@gmail.com>
2026-02-01 15:09:47 +0100

    Improved the user experience
    Added blink animation in CSS

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ function displayPoem(response) {
   new Typewriter("#poem", {
     strings: response.data.answer,
     autoStart: true,
-    delay: 85,
+    delay: 90,
     cursor: "",
   });
 }
@@ -17,6 +17,11 @@ function generatePoem(event) {
   let prompt = `Give me a poem about ${promptInput.value}`;
   let apiKey = "7b8fb87030c4beo42b4207t4f1d8b27a";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="blink">Generating a Dutch poem about ${promptInput.value}…</div>`;
+
   axios.get(apiUrl).then(displayPoem);
 }
 
